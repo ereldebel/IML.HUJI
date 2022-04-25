Fixed pi and var calculation in LDA and naive bayes models. predicted week 3 of challenge.

IMLearn/learners/classifiers/gaussian_naive_bayes.py
@@ -2,6 +2,7 @@ from typing import NoReturn
 from ...base import BaseEstimator
 import numpy as np
 
+
 class GaussianNaiveBayes(BaseEstimator):
     """
     Gaussian Naive-Bayes classifier
@@ -40,14 +41,14 @@ class GaussianNaiveBayes(BaseEstimator):
         y : ndarray of shape (n_samples, )
             Responses of input data to fit to
         """
-        self.classes_ = np.unique(y)
-        bincount = np.bincount(y.astype(int))
-        self.pi_ = bincount / self.classes_.size
+        self.classes_, bincount = np.unique(y, return_counts=True)
+        self.pi_ = bincount / y.size
         sorted_X = X[y.argsort()]
         grouped_X = np.array(
             np.split(sorted_X, np.cumsum(bincount)[:-1], axis=0), dtype=object)
         self.mu_ = np.array([np.mean(group, axis=0) for group in grouped_X])
-        self.vars_ = np.array([np.var(group, axis=0) for group in grouped_X])
+        self.vars_ = np.array(
+            [np.var(group, axis=0, ddof=1) for group in grouped_X])
 
     def _predict(self, X: np.ndarray) -> np.ndarray:
         """

IMLearn/learners/classifiers/linear_discriminant_analysis.py
@@ -47,9 +47,8 @@ class LDA(BaseEstimator):
         y : ndarray of shape (n_samples, )
             Responses of input data to fit to
         """
-        self.classes_ = np.unique(y)
-        bincount = np.bincount(y.astype(int))
-        self.pi_ = bincount / self.classes_.size
+        self.classes_, bincount = np.unique(y,return_counts=True)
+        self.pi_ = bincount / y.size
         sorted_X = X[y.argsort()]
         grouped_X = np.array(
             np.split(sorted_X, np.cumsum(bincount)[:-1], axis=0), dtype=object)