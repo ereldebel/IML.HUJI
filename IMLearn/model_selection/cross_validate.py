b6534676f2bc6427f08d6efca5dd3b5ee144ca18
ereldebel <erel.debel@mail.huji.ac.il>
2022-05-24 12:19:06 +0300

    Implemented cross_validate

--- IMLearn/model_selection/cross_validate.py
+++ IMLearn/model_selection/cross_validate.py
@@ -6,35 +6,47 @@ from IMLearn import BaseEstimator
 
 
 def cross_validate(estimator: BaseEstimator, X: np.ndarray, y: np.ndarray,
-                   scoring: Callable[[np.ndarray, np.ndarray, ...], float], cv: int = 5) -> Tuple[float, float]:
-    """
-    Evaluate metric by cross-validation for given estimator
+                   scoring: Callable[[np.ndarray, np.ndarray, ...], float],
+                   cv: int = 5) -> Tuple[float, float]:
+	"""
+	Evaluate metric by cross-validation for given estimator
 
-    Parameters
-    ----------
-    estimator: BaseEstimator
-        Initialized estimator to use for fitting the data
+	Parameters
+	----------
+	estimator: BaseEstimator
+		Initialized estimator to use for fitting the data
 
-    X: ndarray of shape (n_samples, n_features)
-       Input data to fit
+	X: ndarray of shape (n_samples, n_features)
+	   Input data to fit
 
-    y: ndarray of shape (n_samples, )
-       Responses of input data to fit to
+	y: ndarray of shape (n_samples, )
+	   Responses of input data to fit to
 
-    scoring: Callable[[np.ndarray, np.ndarray, ...], float]
-        Callable to use for evaluating the performance of the cross-validated model.
-        When called, the scoring function receives the true- and predicted values for each sample
-        and potentially additional arguments. The function returns the score for given input.
+	scoring: Callable[[np.ndarray, np.ndarray, ...], float]
+		Callable to use for evaluating the performance of the cross-validated model.
+		When called, the scoring function receives the true- and predicted values for each sample
+		and potentially additional arguments. The function returns the score for given input.
 
-    cv: int
-        Specify the number of folds.
+	cv: int
+		Specify the number of folds.
 
-    Returns
-    -------
-    train_score: float
-        Average train score over folds
+	Returns
+	-------
+	train_score: float
+		Average train score over folds
 
-    validation_score: float
-        Average validation score over folds
-    """
-    raise NotImplementedError()
+	validation_score: float
+		Average validation score over folds
+	"""
+	X_parts = np.array_split(X, cv, axis=1)
+	y_parts = np.array_split(y, cv)
+	train_sum, validation_sum = 0, 0
+	for k in range(cv):
+		X_k_fold = np.concatenate(
+			[part for j, part in enumerate(X_parts) if part != j], axis=1)
+		y_k_fold = np.concatenate(
+			[part for j, part in enumerate(y_parts) if part != j])
+		estimator.fit(X_k_fold, y_k_fold)
+		train_sum += scoring(y_k_fold, estimator.predict(X_k_fold))
+		validation_sum += scoring(y_parts[k], estimator.predict(X_parts[k]))
+	return train_sum / cv, validation_sum / cv
