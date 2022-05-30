c1f858bb0d0d0062622d5d93df75a932d8c773e8
ereldebel <erel.debel@mail.huji.ac.il>
2022-05-30 20:29:46 +0300

    finished implementing questions 3-5 of ex5, implemented ridge regression

--- IMLearn/learners/regressors/ridge_regression.py
+++ IMLearn/learners/regressors/ridge_regression.py
@@ -2,96 +2,106 @@ from __future__ import annotations
 from typing import NoReturn
 from ...base import BaseEstimator
 import numpy as np
+from IMLearn.metrics import mean_square_error
 
 
 class RidgeRegression(BaseEstimator):
-    """
-    Ridge Regression Estimator
+	"""
+	Ridge Regression Estimator
 
-    Solving Ridge Regression optimization problem
-    """
+	Solving Ridge Regression optimization problem
+	"""
 
-    def __init__(self, lam: float, include_intercept: bool = True) -> RidgeRegression:
-        """
-        Initialize a ridge regression model
+	def __init__(self, lam: float,
+	             include_intercept: bool = True) -> RidgeRegression:
+		"""
+		Initialize a ridge regression model
 
-        Parameters
-        ----------
-        lam: float
-            Regularization parameter to be used when fitting a model
+		Parameters
+		----------
+		lam: float
+			Regularization parameter to be used when fitting a model
 
-        include_intercept: bool, default=True
-            Should fitted model include an intercept or not
+		include_intercept: bool, default=True
+			Should fitted model include an intercept or not
 
-        Attributes
-        ----------
-        include_intercept_: bool
-            Should fitted model include an intercept or not
+		Attributes
+		----------
+		include_intercept_: bool
+			Should fitted model include an intercept or not
 
-        coefs_: ndarray of shape (n_features,) or (n_features+1,)
-            Coefficients vector fitted by linear regression. To be set in
-            `LinearRegression.fit` function.
-        """
+		coefs_: ndarray of shape (n_features,) or (n_features+1,)
+			Coefficients vector fitted by linear regression. To be set in
+			`LinearRegression.fit` function.
+		"""
 
+		"""
+		Initialize a ridge regression model
+		:param lam: scalar value of regularization parameter
+		"""
+		super().__init__()
+		self.coefs_ = None
+		self.include_intercept_ = include_intercept
+		self.lam_ = lam
 
-        """
-        Initialize a ridge regression model
-        :param lam: scalar value of regularization parameter
-        """
-        super().__init__()
-        self.coefs_ = None
-        self.include_intercept_ = include_intercept
-        self.lam_ = lam
+	def _fit(self, X: np.ndarray, y: np.ndarray) -> NoReturn:
+		"""
+		Fit Ridge regression model to given samples
 
-    def _fit(self, X: np.ndarray, y: np.ndarray) -> NoReturn:
-        """
-        Fit Ridge regression model to given samples
+		Parameters
+		----------
+		X : ndarray of shape (n_samples, n_features)
+			Input data to fit an estimator for
 
-        Parameters
-        ----------
-        X : ndarray of shape (n_samples, n_features)
-            Input data to fit an estimator for
+		y : ndarray of shape (n_samples, )
+			Responses of input data to fit to
 
-        y : ndarray of shape (n_samples, )
-            Responses of input data to fit to
+		Notes
+		-----
+		Fits model with or without an intercept depending on value of `self.include_intercept_`
+		"""
+		if self.include_intercept_:
+			X = X - X.mean(axis=0)
+			y = y - y.mean()
+		v, vec_sigma, u = np.linalg.svd(X)
+		vec_sigma_l = np.apply_along_axis(
+			lambda x: x / (x * x + self.lam_), 0, vec_sigma)
+		sigma_l = np.zeros((v.shape[0], u.shape[0]))
+		sigma_l[:vec_sigma_l.size, :vec_sigma_l.size] = np.diag(
+			vec_sigma_l)
+		self.coefs_ = v @ sigma_l @ u.T @ y
 
-        Notes
-        -----
-        Fits model with or without an intercept depending on value of `self.include_intercept_`
-        """
-        raise NotImplementedError()
+	def _predict(self, X: np.ndarray) -> np.ndarray:
+		"""
+		Predict responses for given samples using fitted estimator
 
-    def _predict(self, X: np.ndarray) -> np.ndarray:
-        """
-        Predict responses for given samples using fitted estimator
+		Parameters
+		----------
+		X : ndarray of shape (n_samples, n_features)
+			Input data to predict responses for
 
-        Parameters
-        ----------
-        X : ndarray of shape (n_samples, n_features)
-            Input data to predict responses for
+		Returns
+		-------
+		responses : ndarray of shape (n_samples, )
+			Predicted responses of given samples
+		"""
+		return X @ self.coefs_
 
-        Returns
-        -------
-        responses : ndarray of shape (n_samples, )
-            Predicted responses of given samples
-        """
-        raise NotImplementedError()
+	def _loss(self, X: np.ndarray, y: np.ndarray) -> float:
+		"""
+		Evaluate performance under MSE loss function
 
-    def _loss(self, X: np.ndarray, y: np.ndarray) -> float:
-        """
-        Evaluate performance under MSE loss function
+		Parameters
+		----------
+		X : ndarray of shape (n_samples, n_features)
+			Test samples
 
-        Parameters
-        ----------
-        X : ndarray of shape (n_samples, n_features)
-            Test samples
+		y : ndarray of shape (n_samples, )
+			True labels of test samples
 
-        y : ndarray of shape (n_samples, )
-            True labels of test samples
-
-        Returns
-        -------
-        loss : float
-            Performance under MSE loss function
-        """
-        raise NotImplementedError()
+		Returns
+		-------
+		loss : float
+			Performance under MSE loss function
+		"""
+		return mean_square_error(y,self.predict(X))
--- exercises/perform_model_selection.py
+++ exercises/perform_model_selection.py
@@ -3,11 +3,11 @@ import numpy as np
 import pandas as pd
 from sklearn import datasets
 from IMLearn.metrics import mean_square_error
-from IMLearn.utils import split_train_test
 from IMLearn.model_selection import cross_validate
 from IMLearn.learners.regressors import PolynomialFitting, LinearRegression, \
 	RidgeRegression
 from sklearn.linear_model import Lasso
+from sklearn.model_selection import train_test_split
 
 from utils import *
 import plotly.graph_objects as go
@@ -29,39 +29,47 @@ def select_polynomial_degree(n_samples: int = 100, noise: float = 5):
 	# Question 1 - Generate dataset for model f(x)=(x+3)(x+2)(x+1)(x-1)(x-2) + eps for eps Gaussian noise
 	# and split into training- and testing portions
 	f = lambda x: (x + 3) * (x + 2) * (x + 1) * (x - 1) * (x - 2)
-	base_samples = np.linspace(-1.2, 2, n_samples)
-	noise = np.random.normal(0, noise, n_samples)
-	X = f(base_samples) + noise
-	y = f(base_samples)
-	X_dataframe = pd.DataFrame(X).set_index(base_samples)
-	train_X, train_y, test_X, test_y = split_train_test(pd.DataFrame(X),
-	                                                    pd.DataFrame(y), 2 / 3)
-	train_X_index = train_X.index.to_numpy().reshape(-1)
-	train_X = train_X.values.reshape(-1)
-	test_X_index = test_X.index.to_numpy().reshape(-1)
-	test_X = test_X.values.reshape(-1)
-	test_y = test_y.values.reshape(-1)
-	train_y = train_y.values.reshape(-1)
-	fig = go.Figure([go.Scatter(x=base_samples, y=y, mode="lines+markers",
+	noise_vector = np.random.normal(0, noise, n_samples)
+	X = np.linspace(-1.2, 2, n_samples)
+	y = f(X) + noise_vector
+	noiseless_y = f(X)
+	train_X, test_X, train_y, test_y = train_test_split(X, y, test_size=2 / 3)
+	fig = go.Figure([go.Scatter(x=X, y=noiseless_y, mode="lines+markers",
 	                            name="True model"),
-	                 go.Scatter(x=train_X_index, y=train_X, mode="markers",
+	                 go.Scatter(x=train_X, y=train_y,
+	                            mode="markers",
 	                            name="Train samples"),
-	                 go.Scatter(x=test_X_index, y=test_X, mode="markers",
+	                 go.Scatter(x=test_X, y=test_y, mode="markers",
 	                            name="Test samples")], layout=go.Layout(
-		title=r"$\text{True polynomial and noisy samples}$"))
-	# fig.show()
+		title=rf"$\text{{True polynomial and noisy samples. noise = {noise},"
+		      rf" m = {n_samples} samples}}$"))
+	fig.show()
 
 	# Question 2 - Perform CV for polynomial fitting with degrees 0,1,...,10
-	errors = np.ndarray([11, 2],float)
-	for degree in range(11):
-		errors[degree] = cross_validate(PolynomialFitting(degree), train_X, train_y,
+	errors = np.ndarray([11, 2], float)
+	degree_range = range(11)
+	for degree in degree_range:
+		errors[degree] = cross_validate(PolynomialFitting(degree), train_X,
+		                                train_y,
 		                                mean_square_error)
-	print(errors)
-	fig = go.Figure([go.Scatter(x=list(range(11)),y=errors[:,0]),go.Scatter(x=list(range(11)),y=errors[:,1])])
+	minimizer = np.argmin(errors[:, 1])
+	fig = go.Figure(
+		[go.Scatter(x=list(degree_range), y=errors[:, 0], mode="lines+markers",
+		            name="train error"),
+		 go.Scatter(x=list(degree_range), y=errors[:, 1], mode="lines+markers",
+		            name="validation error"),
+		 go.Scatter(x=[minimizer], y=[errors[minimizer, 1]], mode="markers",
+		            name="validation error minimizer")
+		 ], layout=go.Layout(
+			title=rf"$\text{{Mean Train and Validation Errors Using 5-fold"
+			      rf" Cross Validation. noise = {noise}, m = {n_samples} "
+			      rf"samples}}$"))
 	fig.show()
 
 	# Question 3 - Using best value of k, fit a k-degree polynomial model and report test error
-	raise NotImplementedError()
+	model = PolynomialFitting(int(minimizer)).fit(train_X, train_y)
+	print(f"with noise {noise} and {n_samples} samples, k* was {minimizer} and"
+	      f" got a {model.loss(test_X, test_y)} test error.")
 
 
 def select_regularization_parameter(n_samples: int = 50,
@@ -91,3 +99,5 @@ def select_regularization_parameter(n_samples: int = 50,
 if __name__ == '__main__':
 	np.random.seed(0)
 	select_polynomial_degree()
+	select_polynomial_degree(noise=0)
+	select_polynomial_degree(n_samples=1500, noise=10)
